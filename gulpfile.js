f3e14e2a9c0e81aa23c5a8f736d4121f97f7582e
srgg6701 <srgg67@gmail.com>
2014-08-02 11:07:46 +0400

    Добавлен gulp, server.

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp    = require('gulp'),
+    uglify  = require('gulp-uglify'),
+    sass    = require('gulp-ruby-sass');
+gulp.task('styles', function(){
+    gulp.src('scss/**/*.scss')
+        .pipe(sass({
+            style:'compressed'
+        }))
+        .pipe(gulp.dest('app/css/'));
+});
+gulp.task('uglify', function(){
+    gulp.src('js-dev/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('app/js'));
+});
+gulp.task('watch', function(){
+    gulp.watch('js-dev/*.js', ['uglify']);
+});
+gulp.task('default',['watch']);
